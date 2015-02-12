6c7ebf2e72963aee01c12ee45fe902b52ee43daa
Ghost <jmorrisosu@gmail.com>
2015-02-11 20:02:59 -0500

    Added a password finding challenge

---
+++ password.js
@@ -0,0 +1,12 @@
+document.getElementById("passwordButton").onclick = function() {
+
+	if(document.getElementById('passwordText').value == 'mudkips')
+	{
+		alert("Way to go man!");
+	}
+	else
+	{
+		alert("WRONG PASSWORD!  GO AWAY!")
+	}
+
+};
